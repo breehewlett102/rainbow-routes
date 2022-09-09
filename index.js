f1633aec4c5f60435e5b072254198328bc7fea22
Bree Hewlett <breevhew102@brees-mbp.mynetworksettings.com>
2022-09-08 17:25:29 -0700

    Step 3 completed

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const express = require('express')
-const app = express
+require('dotenv').config();
 
-app.length('/', function(req, res){
-    res.send
-})
+const express = require('express')();
+const app = express;
+
+app.get('/', function (req, res) {
+    res.send(` 
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: white;">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <h1>White Page</h1>
+        </body>
+    `)
+});
+
+app.listen(process.env.PORT)

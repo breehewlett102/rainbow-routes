403337fe04ba754d5770beb38dd3b66044910435
Bree Hewlett <breevhew102@brees-mbp.mynetworksettings.com>
2022-09-08 17:36:51 -0700

    Step 5 complete

--- index.js
+++ index.js
@@ -14,4 +14,16 @@ app.get('/', function (req, res) {
     `)
 });
 
+app.get('/:color', function (req, res) {
+    let myColor = req.params.color
+    res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: ${myColor};">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <h1 style="color: ${myColor};">${myColor} Page</h1>
+        </body>
+    `)
+})
+
 app.listen(process.env.PORT)
